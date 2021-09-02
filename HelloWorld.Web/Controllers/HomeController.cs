171690c8b44c10866af7531eba9236ab494b9c1f
StringerJer <89177753+StringerJer@users.noreply.github.com>
2021-09-02 04:29:53 -0400

    Initial commit

---
+++ HelloWorld.Web/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using HelloWorld.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var author = new Author
+            {
+                FirstName = "Hello",
+                LastName = "World",
+                DateOfBirth = DateTimeOffset.UtcNow
+            };
+
+            return View(author);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HelloWorld.Web/Models/Author.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Web.Models
+{
+    public class Author
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTimeOffset DateOfBirth { get; set; }
+    }
+}
